57e3b6f3a845023a2551d558dbbe08b07ccd52cd
Ajit Jadhav <mr.ajitjadhav@gmail.com>
2016-01-15 15:53:23 +0530

    Commiting to push on remote

--- itct.h
+++ itct.h
@@ -1,3 +1,10 @@
+/* Author - Ajit Jadhav
+ * Title  - Functions for ITCT practicals
+ * Date Started  - 04/01/2016
+ */
+ 
 float log2f(float p);
 float info(float p);
-float entrophy(float *arrP, int l);
+float entrophy(float P, int l);
+float efficiency(float h, int avgNumBits);
+float entrophy_ij(float *arrP, int i, int j);
